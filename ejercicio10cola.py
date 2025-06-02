9334a078b2a11716b58155d2e7fb5189dd498566
Lucho0498 <lucianosangalli@gmail.com>
2025-06-02 12:38:08 -0300

    ejercicio 10 y 22 de COLA corregidos

--- ejercicio10cola.py
+++ ejercicio10cola.py
@@ -1,13 +1,20 @@
 from queue import Queue
-
+class notificacionesAplicaciones:
+    def __init__(self, hora, aplicacion, mensaje):
+        self.hora = hora
+        self.aplicacion = aplicacion
+        self.mensaje = mensaje
+    def __str__(self):
+        return f"Hora: {self.hora}, Aplicacion: {self.aplicacion}, Mensaje: {self.mensaje}"
+    
 notificaciones = Queue ()
-notificaciones.arrive({'hora': '10:00', 'aplicacion': 'Facebook', 'mensaje': 'Nuevo comentario en tu foto'})
-notificaciones.arrive({'hora': '10:05', 'aplicacion': 'Twitter', 'mensaje': 'Nuevo seguidor'})
-notificaciones.arrive({'hora': '10:10', 'aplicacion': 'Facebook', 'mensaje': 'Alguien reaccionó a tu publicación'})
-notificaciones.arrive({'hora': '11:30', 'aplicacion': 'TikTok', 'mensaje': 'Nuevo comentario en tu video'})
-notificaciones.arrive({'hora': '13:27', 'aplicacion': 'Instagram', 'mensaje': 'Alguien reaccionó a tu publicación'})
-notificaciones.arrive({'hora': '15:30', 'aplicacion': 'Twitter', 'mensaje': 'Alguien reaccionó a tu publicación'})
-notificaciones.arrive({'hora': '16:10', 'aplicacion': 'Instagram', 'mensaje': 'A este usuario le gustó tu historia'})
+notificaciones.arrive(notificacionesAplicaciones('10:00', 'Facebook', 'Nuevo comentario en tu foto'))
+notificaciones.arrive(notificacionesAplicaciones('10:05', 'Twitter', 'Nuevo seguidor'))
+notificaciones.arrive(notificacionesAplicaciones('10:10', 'Facebook', 'Alguien reaccionó a tu publicación'))
+notificaciones.arrive(notificacionesAplicaciones('11:30', 'TikTok', 'Nuevo comentario en tu video'))
+notificaciones.arrive(notificacionesAplicaciones('13:27', 'Instagram', 'Alguien reaccionó a tu publicación'))
+notificaciones.arrive(notificacionesAplicaciones('15:30', 'Twitter', 'Alguien reaccionó a tu publicación'))
+notificaciones.arrive(notificacionesAplicaciones('16:10', 'Instagram','A este usuario le gustó tu historia'))
 
 
 
@@ -16,7 +23,7 @@ def eliminarNotificacionesFacebook(notificaciones):
     colaAux =  Queue()
     while notificaciones.size() > 0:
         notificacion = notificaciones.attention()
-        if notificacion['aplicacion'] != 'Facebook':
+        if notificacion.aplicacion != 'Facebook':
             colaAux.arrive(notificacion)
 
     return colaAux
@@ -26,8 +33,8 @@ def MostrarNotificacionesTwitter(notificaciones):
     colaAux = Queue()
     while notificaciones.size() > 0:
         notificacion = notificaciones.attention()
-        if notificacion['aplicacion'] == "Twitter":
-            print(notificacion['mensaje'])
+        if notificacion.aplicacion == "Twitter":
+            print(notificacion.mensaje)
         colaAux.arrive(notificacion)
     
     while colaAux.size() > 0:
@@ -37,7 +44,7 @@ def AlmacenarNotificacionSegunHora(notificaciones):
     colaAux = Queue()
     while notificaciones.size() > 0:
         notificacion = notificaciones.attention()
-        if '11:45' <= notificacion['hora'] <= '15:57':
+        if '11:45' <= notificacion.hora <= '15:57':
             colaAux.arrive(notificacion)
     
     while colaAux.size() > 0:
